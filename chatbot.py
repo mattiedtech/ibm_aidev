5ace2777ff4fd32d644708c090cf678d7325a639
mattiedtech <mattieearleen@gmail.com>
2025-10-12 15:39:05 -0400

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,33 @@
+# Import req'd tools from transformers library
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+# Define model and tokenizer
+model_name = "facebook/blenderbot-400M-distill"
+# Load model (download on first run and reference local installation for consequent runs)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+# Initialize conversation history
+conversation_history = []
+
+while True:
+    # Create conversation history string
+    history_string = "\n".join(conversation_history)
+
+    # Get input from user
+    input_text = input("> ")
+
+    #Tokenize the input text and history
+    inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
+
+    # Generate the response from the model
+    outputs = model.generate(**inputs)
+
+    # Decode response
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
+
+    print(response)
+
+    # Add interaction to conversation history
+    conversation_history.append(input_text)
+    conversation_history.append(response)
